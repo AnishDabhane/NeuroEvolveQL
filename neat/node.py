75bbc4d361afcae3d92c520458b83a1edfbc912e
anishdabhane <anishdabhane@gmail.com>
2024-04-09 13:36:12 +0530

    comments added

--- neat/node.py
+++ neat/node.py
@@ -3,17 +3,17 @@ import math
 
 class Node:
     def __init__(self, number, layer):
-        # Number in the network
+        # Each node has a unique number associated with it
         self.number = number
-        # On Layer
+        # defines to which layer does the node belong
         self.layer = layer
-        # for weighted sum
+        # for weighted sum of all the incoming genes(connections)
         self.sum = 0 
         # for output =sigmoid(sum)
-        self.output=0 # for output ofcourse
+        self.output=0 
         # for storing certain value required during backpropogation
         self.value=0 
-        # all genes coming to this node
+        # list of all genes incoming genes to a node
         self.in_genes = []
         # Sigmoid activation
         self.sigmoid = lambda x : 1 / (1 + math.exp(-x))
@@ -26,26 +26,26 @@ class Node:
         self.pos = [0, 0]
         pass
 
-    # Clone the node
+    # returns a clone of the node
     def clone(self):
         n = Node(self.number, self.layer)
         n.output = self.output
         n.pos = self.pos
         return n
 
-    # Calculate output value
+    # Calculates the sum and output at particular node
     def calculate(self):
-        if self.layer == 0:
+        if self.layer == 0:  # layer = 0 -> input layer
             print('No calculations for first layer')
             return
 
         s = 0
-        for g in self.in_genes:
-            if g.enabled:
+        for g in self.in_genes:  # in_genes contains all the incoming genes
+            if g.enabled:  #  genes can be enabled/disabled while mutating
                 # weighted sum 
-                s += g.in_node.output * g.weight
+                s += (g.in_node.output * g.weight)
         self.sum = s
-        # applying activation function to weighted sum to calucate its output
+        # applying activation function to weighted sum to calucate its output,and rounding of to 4 decimal places
         self.output = round(self.sigmoid(self.sum),4)
         pass
 
